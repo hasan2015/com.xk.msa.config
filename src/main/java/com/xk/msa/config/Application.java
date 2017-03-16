19bf5dd83169aef9d74acd86d23669136b92c03e
yanhaixun <yanhaixun@360ser.com>
2017-03-16 13:43:39 +0800

    增加了caservice，并且通过ui代理能够生成token，getuserinfo验证通过。 下一步先验证一下跨域问题

--- src/main/java/com/xk/msa/config/Application.java
+++ src/main/java/com/xk/msa/config/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigApplication {
+public class Application {
 	
 	 public static void main(String[] args) {
-	        SpringApplication.run(ConfigApplication.class, args);
+	        SpringApplication.run(Application.class, args);
 	    }
 
 }
